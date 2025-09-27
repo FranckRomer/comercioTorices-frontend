c9a214ad6380ab5bcc2bdfe5f806b6e91f559778
FranckRomer <paco_metal95@hotmail.com>
2025-09-26 19:37:19 -0600

    cambio de tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/postcss';
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
   plugins: [],
-  integrations: [tailwind()],
 }
 
 
